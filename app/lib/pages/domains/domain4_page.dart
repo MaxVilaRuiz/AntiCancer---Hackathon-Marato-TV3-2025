95f693caaecf2b870c8a5de97ce9f1db2611737b
Max-Gimeno-G <max.gimeno.g@gmail.com>
2025-12-13 14:17:52 +0100

    Start of the domain4 config

--- app/lib/pages/domains/domain4_page.dart
+++ app/lib/pages/domains/domain4_page.dart
@@ -1,20 +1,107 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Domain4Page extends StatelessWidget {
-  const Domain4Page({super.key});
+
+
+class Dom4Page extends StatefulWidget {
+  const Dom4Page({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Domain 4'),
-      ),
-      body: const Center(
-        child: Text(
-          'Contenido de Domain 4',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+  State<Dom4Page> createState() => Page4();
+};
+
+class Page4 extends State<Dom4Page>{
+    bool showStartButton = true;
+    List<List<int> > matrix = List.generate(
+    4,
+    (_) => List.filled(5, 0), 
     );
-  }
-}
+
+    int buttonAct = 0;
+
+    final Random random = Random();
+
+    @override
+
+    void clearMatrix (){
+        for(int i = 0; i<4; i++){
+            for(int j = 0; j<5; j++){
+                matrix[i] [j] = 0;
+            }
+        }
+    }
+    
+
+    void positionNumbers (){
+        for(int i = 1; i<11; i++){
+            int r = random.nextInt(20);
+            if(matrix[r/4] [r%5] == 0){
+                matrix[r/4] [r%5] = i;
+            }
+            else{
+                i--;
+            }
+        }
+    }
+
+    @override
+
+    class ButtonNum {
+
+        final int value;
+        bool active = true;
+
+        @override
+
+        bjuttonNum(this.value);
+
+        void check (){
+
+            if(this.value == buttonAct - 1){
+                
+                this.active = false;
+            }
+
+        }
+
+        @override
+        Widget build(BuildContext context) {
+        return Scaffold(
+            body: Center(
+            child: active
+                ? ElevatedButton(
+                    onPressed: () {
+                        setState(() {
+                        active = false; // desapareix el botó
+                        });
+                    },
+                    child: const Text('$value'),
+                )
+            ),
+        );
+        }
+    };
+
+
+    @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Dom4')),
+        body: Center(
+        child: showStartButton
+            ? ElevatedButton(
+                onPressed: () {
+                    setState(() {
+                    showStartButton = false; // desapareix el botó
+                    clearMatrix();
+                    positionNumbers(); // inicialitza la matriu
+                    });
+                },
+                child: const Text('Començar Prova'),
+                )
+        ),
+    );
+    }
+
+
+};
