f5297f7c767b21db350ebc62da5bbb0bebfa8b36
Max Vilà Ruiz <max16506@gmail.com>
2025-12-14 07:51:56 +0100

    Add predefined subjective mesure questionnaires for testing

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'widgets/main_scaffold.dart';
 
-void main() {
+Future<void> main() async {
+  // TODO: Delete this part to not initialize with predefined questionnaires (only for testing)
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final predefinedQuestionnaires = [
+    '{"date":"2025-12-13","diagnosis":"Atenció"}',
+    '{"date":"2025-12-12","diagnosis":"Fluència verbal"}',
+    '{"date":"2025-12-11","diagnosis":"Velocitat de processament"}',
+    '{"date":"2025-12-10","diagnosis":"Memòria"}',
+    '{"date":"2025-12-09","diagnosis":"Velocitat de processament"}',
+    '{"date":"2025-12-08","diagnosis":"Fluència verbal"}',
+  ];
+
+  final prefs = await SharedPreferences.getInstance();
+
+  // Only store if they do not exist
+  if (!prefs.containsKey('daily_entries')) {
+    await prefs.setStringList('daily_entries', predefinedQuestionnaires);
+  }
+
   runApp(const MyApp());
 }
 
